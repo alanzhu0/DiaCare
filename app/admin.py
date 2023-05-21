6f7313dca3d3987595e9bad9d3f51e9a96e996a0
Alan Zhu <2025azhu@tjhsst.edu>
2023-05-20 20:40:43 -0400

    Better admin interfaces

--- app/admin.py
+++ app/admin.py
@@ -1,18 +1,170 @@
 from django.contrib import admin
-from .models import *
+from django.conf import settings
+from .models import User, Food, Produce, FoodChoice, ProduceChoice, ProduceCategory, Doctor, Dietician, Order
 
 admin.site.site_header = "DiaCare Administration"
 
-admin.site.register(User)
+def to_str(obj):
+    return str(obj)
 
-admin.site.register(Doctor)
-admin.site.register(Dietician)
+class UserAdmin(admin.ModelAdmin):
+    def admin(obj):
+        return obj.is_superuser or obj.is_staff
+    admin.boolean = True
+    
+    to_str.short_description = 'Full Name (First Middle Last)'
+    list_display = (
+        to_str,
+        'email',
+        'gender',
+        'doctor',
+        'dietician',
+        'last_clinic_visit',
+        'last_food_received',
+        'is_active',
+        admin,
+        'user_comments',
+        'medical_comments',
+        'admin_comments',
+    )
+    
+    list_filter = (
+        'gender',
+        'doctor',
+        'dietician',
+        'last_clinic_visit',
+        'last_food_received',
+        'is_active',
+    )
+    
+    search_fields = (
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'user_comments',
+        'medical_comments',
+        'admin_comments',
+    )
+    
+class DoctorDieticianAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'phone',
+    )
+    
+    search_fields = (
+        'name',
+        'email',
+        'phone',
+    )
 
-admin.site.register(Food)
-admin.site.register(FoodChoice)
+class FoodChoiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'quantity',
+        'active',
+    )
+    
+    list_filter = (
+        'active',
+    )
+    
+    search_fields = (
+        'name',
+        'quantity',
+    )
+    
+    actions = (
+        'mark_active',
+        'mark_inactive',
+    )
+    
+    @admin.action(description="Mark selected choices as active")
+    def mark_active(self, request, queryset):
+        queryset.update(active=True)
+    
+    @admin.action(description="Mark selected choices as inactive")
+    def mark_inactive(self, request, queryset):
+        queryset.update(active=False)
+    
+class ProduceChoiceAdmin(FoodChoiceAdmin):
+    list_display = (
+        'name',
+        'quantity',
+        'category',
+        'active',
+    )
+    
+    list_filter = (
+        'active',
+        'category',
+    )
+    
+class ProduceCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'maximum_choices',
+    )
+    
+    search_fields = (
+        'name',
+    )
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return self.readonly_fields + ('id',)
+        return self.readonly_fields
 
-admin.site.register(Produce)
-admin.site.register(ProduceChoice)
-admin.site.register(ProduceCategory)
+class OrderAdmin(admin.ModelAdmin):
+    def fulfilled(obj):
+        return obj.fulfilled
+    fulfilled.boolean = True
+    
+    list_display = (
+        'user',
+        'date_ordered',
+        'date_scheduled',
+        'date_fulfilled',
+        fulfilled,
+        'type',
+    )
+    
+    list_filter = (
+        'type',
+        'date_ordered',
+        'date_scheduled',
+        'date_fulfilled',
+        'user',
+    )
+    
+    search_fields = (
+        'user',
+    )
+    
+    actions = (
+        'mark_fulfilled',
+    )
+    
+    @admin.action(description="Mark selected orders as fulfilled")
+    def mark_fulfilled(self, request, queryset):
+        queryset.update(date_fulfilled=timezone.now())
+    
 
-admin.site.register(Order)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Doctor, DoctorDieticianAdmin)
+admin.site.register(Dietician, DoctorDieticianAdmin)
+
+admin.site.register(FoodChoice, FoodChoiceAdmin)
+admin.site.register(ProduceChoice, ProduceChoiceAdmin)
+
+admin.site.register(ProduceCategory, ProduceCategoryAdmin)
+
+admin.site.register(Order, OrderAdmin)
+
+if settings.DEBUG:
+    admin.site.register(Food)
+    admin.site.register(Produce)
+
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.models import UserManager as DjangoUserManager
 from django.db import models
+from django.utils import timezone
+
 
 
 # Create your models here.
@@ -27,25 +28,33 @@ class User(AbstractBaseUser, PermissionsMixin):
     gender = models.CharField(choices=(
         ("male", "Male"),
         ("female", "Female"),
-        ("other", "Other"),
+        ("nonbinary", "Non-binary"),
     ), max_length=255, blank=True)
+    
+    is_active = models.BooleanField(
+        default=True, 
+        help_text="Whether the user is currently enrolled in the program and is authorized to log in and order food.", 
+        verbose_name="Active"
+    )
 
     address = models.CharField(max_length=255, blank=True)
 
-    doctor = models.ForeignKey(
-        'Doctor', on_delete=models.CASCADE, blank=True, null=True)
-    dietician = models.ForeignKey(
-        'Dietician', on_delete=models.CASCADE, blank=True, null=True)
+    doctor = models.ForeignKey('Doctor', on_delete=models.CASCADE, blank=True, null=True)
+    dietician = models.ForeignKey('Dietician', on_delete=models.CASCADE, blank=True, null=True)
 
     last_clinic_visit = models.DateTimeField(blank=True, null=True)
     last_food_received = models.DateTimeField(blank=True, null=True)
     next_clinic_visit = models.DateTimeField(blank=True, null=True)
     next_food_batch = models.DateTimeField(blank=True, null=True)
+    
+    user_comments = models.TextField(blank=True, help_text="Comments from the patient")
+    medical_comments = models.TextField(blank=True, help_text="Comments from the doctor, dietician, or clinic regarding the patient's medical conditions")
+    admin_comments = models.TextField(blank=True, help_text="Comments from DiaCare administrators")
 
     USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.middle_name + ' ' if self.middle_name else ''}{self.last_name}"
 
 
 class Doctor(models.Model):
@@ -82,7 +91,7 @@ class ProduceChoice(models.Model):
     """Admin-defined produce choices"""
     name = models.CharField(max_length=255)
     quantity = models.CharField(max_length=255, default="1")
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True,  help_text="Whether this produce choice is currently available for selection")
     category = models.ForeignKey('ProduceCategory', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -94,13 +103,13 @@ class ProduceChoice(models.Model):
 
 class ProduceCategory(models.Model):
     """Admin-defined produce categories"""
-    id = models.CharField(max_length=255, primary_key=True)
+    id = models.CharField(max_length=255, primary_key=True, help_text="Unique identifier for this category")
     name = models.CharField(max_length=255)
     maximum_choices = models.IntegerField(
         default=1,
         help_text="Maximum number of produce choices allowed in this category"
     )
-    display_order = models.IntegerField(default=0)
+    display_order = models.IntegerField(default=0, help_text="Order in which this category should be displayed to users")
 
     @property
     def produces(self):
@@ -111,6 +120,7 @@ class ProduceCategory(models.Model):
 
     class Meta:
         ordering = ['display_order', 'name']
+        verbose_name_plural = "Produce categories"
 
 
 class Food(models.Model):
@@ -129,7 +139,7 @@ class FoodChoice(models.Model):
     """Admin-defined food choices"""
     name = models.CharField(max_length=255)
     quantity = models.CharField(max_length=255, default="1")
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True, help_text="Whether this food choice is currently available for selection")
 
     def __str__(self):
         return f"{self.name} ({self.quantity}) - Inactive" if not self.active else f"{self.name} ({self.quantity})"
@@ -140,14 +150,17 @@ class FoodChoice(models.Model):
 
 class Order(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
-    complete = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now_add=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
     date_scheduled = models.DateTimeField(blank=True, null=True)
     date_fulfilled = models.DateTimeField(blank=True, null=True)
     type = models.CharField(choices=(
         ("pickup", "Pickup"),
         ("delivery", "Delivery"),
     ), max_length=255, default="Pickup")
+    
+    @property
+    def fulfilled(self):
+        return self.date_fulfilled is not None and self.date_fulfilled < timezone.now()
 
     @property
     def foods(self):
@@ -158,7 +171,7 @@ class Order(models.Model):
         return Produce.objects.filter(order=self)
 
     def __str__(self):
-        return f"{self.user}: {self.date}"
+        return f"Order by {self.user} on {self.date_ordered.strftime('%a, %b %d %Y at %I:%M:%S %p')}"
 
     class Meta:
-        ordering = ['-date']
+        ordering = ['-date_scheduled']
